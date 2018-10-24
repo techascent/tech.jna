d8210dd3e380aa2fa34f3735e148136a3c79a553
Chris Nuernberger <chris@techascent.com>
2018-10-24 13:51:25 -0600

    first commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# tech.jna
+
+Copyright © 2018 TechAscent, LLC
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tech.jna "0.1.0-SNAPSHOT"
+  :description "Bindings of tech.datatype system to jna."
+  :url "http://github.com/tech-ascent/tech.jna"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
