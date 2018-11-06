bebbc9fb57694706465bf11dc268f0317e3380ce
Chris Nuernberger <chris@thinktopic.com>
2018-11-05 17:27:56 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.jna "1.11"
+(defproject techascent/tech.jna "1.12-SNAPSHOT"
   :description "Bindings of tech.datatype system to jna."
   :url "http://github.com/tech-ascent/tech.jna"
   :license {:name "Eclipse Public License"
