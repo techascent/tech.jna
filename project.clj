0c459e496e371248c3d1213cf392d458346182db
Chris Nuernberger <chris@thinktopic.com>
2018-11-05 17:26:38 -0700

    1.11

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.java.dev.jna/jna "5.0.0"]
-                 [techascent/tech.datatype "1.4-SNAPSHOT"]
+                 [techascent/tech.datatype "1.4"]
                  [techascent/tech.resource "2.0"]]
   :profiles {:dev {:dependencies [[com.taoensso/timbre "4.10.0"]]}})
