57468e1fe592a5c319e15289b690398128f70625
Chris Nuernberger <chris@techascent.com>
2018-11-18 10:17:21 -0700

    Removed datatype bindings.  This library will now be a root of datatype.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject techascent/tech.jna "1.14-SNAPSHOT"
+(defproject techascent/tech.jna "2.0-SNAPSHOT"
   :description "Bindings of tech.datatype system to jna."
   :url "http://github.com/tech-ascent/tech.jna"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.java.dev.jna/jna "5.0.0"]
-                 [techascent/tech.datatype "1.5"]
                  [techascent/tech.resource "3.0"]]
   :profiles {:dev {:dependencies [[com.taoensso/timbre "4.10.0"]]}})
