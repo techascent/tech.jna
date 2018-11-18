e3c12bdadda604ca4014b694fc7fa75fc157f835
Chris Nuernberger <chris@techascent.com>
2018-11-18 12:03:21 -0700

    2.4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject techascent/tech.jna "2.4-SNAPSHOT"
+(defproject techascent/tech.jna "2.4"
   :description "Bindings of tech.datatype system to jna."
   :url "http://github.com/tech-ascent/tech.jna"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.java.dev.jna/jna "5.0.0"]
-                 [techascent/tech.resource "3.0"]]
+                 [techascent/tech.resource "3.1"]]
   :profiles {:dev {:dependencies [[com.taoensso/timbre "4.10.0"]]}})
