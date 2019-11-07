333b5b2bd29e940d612d6fac1e34e1539d6f719f
Chris Nuernberger <chris@techascent.com>
2019-11-07 09:35:54 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.jna "3.19"
+(defproject techascent/tech.jna "3.20-SNAPSHOT"
   :description "Bindings of tech.datatype system to jna."
   :url "http://github.com/tech-ascent/tech.jna"
   :license {:name "Eclipse Public License"
