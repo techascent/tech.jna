d2f81480416a4dcaa39dff2f5e59e88de90d550a
Chris Nuernberger <chris@techascent.com>
2021-12-28 11:16:50 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.jna "4.06"
+(defproject techascent/tech.jna "4.07-SNAPSHOT"
   :description "Bindings to JNA.  Used with libpython-clj among others."
   :url "http://github.com/tech-ascent/tech.jna"
   :license {:name "Eclipse Public License"
